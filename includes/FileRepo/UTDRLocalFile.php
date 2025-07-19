c5742acae671cb2e3da34c9e9065f78353dd3da9
Luka Simić <git@kocka.tech>
2025-07-19 16:55:07 +0200

    Fix MediaViewer not displaying some images.

--- includes/FileRepo/UTDRLocalFile.php
+++ includes/FileRepo/UTDRLocalFile.php
@@ -22,11 +22,23 @@ class UTDRLocalFile extends LocalFile {
 	 * Adds a suffix to an image/thumbnail URL for cache busting. When a new
 	 * file is uploaded, the SHA1 hash of the file changes, and users should
 	 * get the new version of the file instead.
+	 *
+	 * Because MediaWiki's mw.util.parseImageUrl function is extremely stupid
+	 * and doesn't work properly after we add this cache buster, MediaViewer
+	 * stops displaying some of the images on the wiki. So to do this, we add
+	 * two more query parameters that make that function think we're actually
+	 * using a thumb.php URL and correctly parse the filename.
+	 *
+	 * @param string $url URL to the file's image
 	 * @return string
 	 */
 	private function addCacheBuster( string $url ): string {
 		return wfAppendQuery( $url, [
-			'cb' => substr( $this->getSha1(), 0, 6 )
+			'cb' => substr( $this->getSha1(), 0, 6 ),
+			// This works because mw.util.parseImageUrl takes a different code
+			// path when the URL matches /thumb\.php/.
+			'h' => 'thumb.php',
+			'f' => $this->getName(),
 		] );
 	}
 
