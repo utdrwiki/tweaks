ca56d9eaa09e4af0c2f4fea6fffbbc5527572ae9
Luka Simić <git@kocka.tech>
2025-11-01 19:36:36 +0100

    Fix foreign file repository issues on DR wikis.

--- includes/FileRepo/UTDRForeignDBRepo.php
+++ includes/FileRepo/UTDRForeignDBRepo.php
@@ -15,4 +15,25 @@ class UTDRForeignDBRepo extends ForeignDBRepo {
 		$this->fileFromRowFactory = [ UTDRForeignDBFile::class, 'newFromRow' ];
 		parent::__construct( $info );
 	}
+
+	/**
+	 * MediaWiki's FileSelectQueryBuilder queries the actor table for the wiki
+	 * that the file belongs to. This is a problem on Deltarune Wiki, because
+	 * our actor table is shared and resides on the Undertale Wiki instead.
+	 * @return callable New database factory
+	 */
+	protected function getDBFactory() {
+		$parentFunc = parent::getDBFactory();
+		return static function ( $index ) use ( $parentFunc ) {
+			$db = $parentFunc( $index );
+			$db->setTableAliases( [
+				'actor' => [
+					'dbname' => 'en_undertale',
+					'schema' => '',
+					'prefix' => '',
+				]
+			] );
+			return $db;
+		};
+	}
 }
