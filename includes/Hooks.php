3a96cdd5cfa80367853b562cbff57511a1be604d
Luka Simić <git@kocka.tech>
2026-04-23 16:48:16 +0200

    Remove TemplateStyles modifications. Moved to a new extension: https://www.mediawiki.org/wiki/Extension:TemplateStylesUnlimited

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -9,7 +9,7 @@ use MediaWiki\Context\IContextSource;
 use MediaWiki\Extension\AbuseFilter\FilterUser;
 use MediaWiki\Extension\UTDRTweaks\Interwiki\UTDRInterwikiLookup;
 use MediaWiki\Extension\UTDRTweaks\Preferences\UTDRPreferencesFactory;
-use MediaWiki\FileRepo\File;
+use MediaWiki\FileRepo\File\File;
 use MediaWiki\Hook\ImageBeforeProduceHTMLHook;
 use MediaWiki\Html\Html;
 use MediaWiki\Interwiki\ClassicInterwikiLookup;
@@ -23,9 +23,6 @@ use MediaWiki\Status\Status;
 use MediaWiki\Title\Title;
 use MediaWiki\User\User;
 use MediaWiki\WikiMap\WikiMap;
-use Wikimedia\CSS\Grammar\MatcherFactory;
-use Wikimedia\CSS\Sanitizer\StylesheetSanitizer;
-use Wikimedia\CSS\Sanitizer\StylePropertySanitizer;
 
 class Hooks implements ImageBeforeProduceHTMLHook {
 	private const CAPTION_REQUIRED_MEDIA_TYPES = [
@@ -94,7 +91,7 @@ class Hooks implements ImageBeforeProduceHTMLHook {
 
 	/**
 	 * Fall back to a file's page title whenever an alt text is missing.
-	 * @param null $unused Will always be null
+	 * @param $unused Will always be null
 	 * @param Title &$title Title object of the image
 	 * @param File|false &$file File object, or false if it doesn't exist
 	 * @param array &$frameParams Various parameters with special meanings; see documentation in
@@ -189,34 +186,6 @@ class Hooks implements ImageBeforeProduceHTMLHook {
 			'',
 			$pageText
 		);
-	}
-
-	/**
-	 * Prevents the TemplateStyles content model from being used outside of
-	 * the MediaWiki namespace, because it is completely unrestricted in our
-	 * implementation.
-	 * @param mixed $modelId Content mode ID
-	 * @param Title $title Title object of the page being edited
-	 * @param mixed $ok Whether the content model can be used
-	 * @return void|bool False to abort further processing
-	 */
-	public static function onContentModelCanBeUsedOn( $modelId, Title $title, &$ok ) {
-		if ( $modelId === 'sanitized-css' && !$title->isSiteCssConfigPage() ) {
-			$ok = false;
-			return false;
-		}
-	}
-
-	/**
-	 * Replace TemplateStyles's sanitizer with a no-op sanitizer.
-	 * @param StylesheetSanitizer $sanitizer
-	 * @param StylePropertySanitizer $propertySanitizer
-	 * @param MatcherFactory $matcherFactory
-	 * @return false To abort further processing
-	 */
-	public static function onTemplateStylesStylesheetSanitizer( StylesheetSanitizer &$sanitizer, StylePropertySanitizer $propertySanitizer, MatcherFactory $matcherFactory ): bool {
-		$sanitizer = new NoopStylesheetSanitizer();
-		return false;
 	}
 
 	/**
