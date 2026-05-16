fdebbe0777709940b9e87138cc68913abf9bf5e8
Luka Simić <git@kocka.tech>
2026-05-16 15:25:03 +0200

    Make batchProcessFiles fix file sizes.

--- maintenance/batchProcessFiles.php
+++ maintenance/batchProcessFiles.php
@@ -3,8 +3,11 @@
 namespace MediaWiki\Extension\UTDRTweaks\Maintenance;
 
 use MediaWiki\FileRepo\File\FileSelectQueryBuilder;
+use MediaWiki\FileRepo\File\LocalFile;
+use MediaWiki\FileRepo\LocalRepo;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\Maintenance\Maintenance;
+use MediaWiki\Shell\CommandFactory;
 use Wikimedia\Rdbms\SelectQueryBuilder;
 use function count;
 use function Wikimedia\base_convert;
@@ -25,6 +28,9 @@ require_once "$IP/maintenance/Maintenance.php";
  * optimization using the `optipng -o5 -strip all [image]` command.
  */
 class BatchProcessFiles extends Maintenance {
+	private LocalRepo $repo;
+	private CommandFactory $cmdFactory;
+
 	public function __construct() {
 		parent::__construct();
 		$this->addDescription( 'A maintenance script for batch processing files based on selection criteria.' );
@@ -65,9 +71,11 @@ class BatchProcessFiles extends Maintenance {
 		$maxSize = $this->getOption( 'max-size', 1 * 1024 * 1024 );
 		$mimeType = $this->getOption( 'mime-type' );
 
-		if ( $operation !== 'optimize' ) {
-			$this->fatalError( "Unsupported operation: $operation\n" );
-		}
+		$services = $this->getServiceContainer();
+		$this->repo = $services->getRepoGroup()->getLocalRepo();
+		$this->cmdFactory = $services->getShellCommandFactory();
+		$logger = LoggerFactory::getInstance( 'BatchProcessFiles' );
+		$this->cmdFactory->setLogger( $logger );
 
 		// Create a backup directory if it doesn't exist.
 		$backupDir = wfTempDir() . '/file_backup';
@@ -80,7 +88,6 @@ class BatchProcessFiles extends Maintenance {
 
 		// Build the selection criteria.
 		$dbr = $this->getReplicaDB();
-		$dbw = $this->getPrimaryDB();
 		$start = '';
 		$conds = [
 			$dbr->expr( 'img_name', '>', '' ),
@@ -99,11 +106,6 @@ class BatchProcessFiles extends Maintenance {
 
 		// Process files in batches.
 		$numChanged = 0;
-		$services = $this->getServiceContainer();
-		$repo = $services->getRepoGroup()->getLocalRepo();
-		$cmdFactory = $services->getShellCommandFactory();
-		$logger = LoggerFactory::getInstance( 'BatchProcessFiles' );
-		$cmdFactory->setLogger( $logger );
 		do {
 			$conds[0] = $dbr->expr( 'img_name', '>', $start );
 			$res = FileSelectQueryBuilder::newForFile( $dbr )
@@ -113,74 +115,115 @@ class BatchProcessFiles extends Maintenance {
 				->caller( __METHOD__)
 				->fetchResultSet();
 			foreach ( $res as $row ) {
-				$file = $repo->newFileFromRow( $row );
 				$start = $row->img_name;
-				$this->output( "Processing {$row->img_name}...\n" );
-				$name = $file->getName();
-				$path = $file->getPath();
-				$tempFilePath = preg_replace(
-					'/[\\\\&!?"<>%^{};,*[\\]]/',
-					'__',
-					"$backupDir/$name",
-				);
-				$size = $repo->getFileSize( $path );
-				if ( $size === false ) {
-					$this->output( "{$row->img_name} does not exist.\n" );
-					continue;
+				$file = $this->repo->newFileFromRow( $row );
+				switch ( $operation ) {
+					case 'optimize':
+						$changed = $this->optimize( $file, $backupDir, $dryRun );
+						break;
+					case 'fix-filesize':
+						$changed = $this->fixFileSize( $file, $dryRun );
+						break;
+					default:
+						$this->fatalError( "Unsupported operation: $operation\n" );
 				}
-				if ( $size != $row->img_size ) {
-					$this->output( "{$row->img_name} size has changed since selection; skipping.\n" );
-					continue;
+				if ( $changed ) {
+					++$numChanged;
 				}
-				$oldSha1 = $file->getSha1();
-				$filePath = $file->getLocalRefPath();
-				$result = $cmdFactory
-					->createBoxed( 'UTDRTweaks' )
-					->params(
-						'optipng',
-						'-o5',
-						'-strip=all',
-						'-preserve',
-						'-fix',
-						'-out=output.png',
-						'--',
-						'input.png',
-					)
-					->inputFileFromFile( 'input.png', $filePath )
-					->outputFileToFile( 'output.png', $tempFilePath )
-					->firejailDefaultSeccomp()
-					->disableNetwork()
-					->routeName( 'UTDRTweaks-optipng' )
-					->execute();
-				if ( $result->getExitCode() !== 0 ) {
-					$this->error( "Failed to process {$row->img_name}:\nout: {$result->getStdout()}\nerr: {$result->getStderr()}\n" );
-					continue;
-				}
-				$newSha1 = base_convert( sha1_file( $tempFilePath ), 16, 36, 31 );
-				if ( $oldSha1 === $newSha1 ) {
-					$this->output( "{$row->img_name} unchanged after processing; skipping update.\n" );
-					@unlink( $tempFilePath );
-					continue;
-				}
-				if ( !$dryRun ) {
-					// TODO: Update for the new file table schema.
-					$dbw->newUpdateQueryBuilder()
-						->update( 'image' )
-						->set( [ 'img_sha1' => $newSha1 ] )
-						->where([ 'img_name' => $row->img_name ])
-						->caller( __METHOD__ )
-						->execute();
-					$status = $repo->quickImport( $tempFilePath, $path );
-					if ( !$status->isGood() ) {
-						$this->error( $status );
-						continue;
-					}
-				}
-				++$numChanged;
 			}
 		} while ( $res->numRows() );
 
 		$this->output( "Files changed: $numChanged\n" );
+	}
+
+	private function optimize( LocalFile $file, string $backupDir, bool $dryRun ): bool {
+		$fileName = $file->getName();
+		$this->output( "Processing $fileName...\n" );
+		$name = $file->getName();
+		$path = $file->getPath();
+		$tempFilePath = preg_replace(
+			'/[\\\\&!?"<>%^{};,*[\\]]/',
+			'__',
+			"$backupDir/$name",
+		);
+		$size = $this->repo->getFileSize( $path );
+		if ( $size === false ) {
+			$this->output( "$fileName does not exist.\n" );
+			return false;
+		}
+		if ( $size != $file->getSize() ) {
+			$this->output( "$fileName size has changed since selection; skipping.\n" );
+			return false;
+		}
+		$oldSha1 = $file->getSha1();
+		$filePath = $file->getLocalRefPath();
+		$result = $this->cmdFactory
+			->createBoxed( 'UTDRTweaks' )
+			->params(
+				'optipng',
+				'-o5',
+				'-strip=all',
+				'-preserve',
+				'-fix',
+				'-out=output.png',
+				'--',
+				'input.png',
+			)
+			->inputFileFromFile( 'input.png', $filePath )
+			->outputFileToFile( 'output.png', $tempFilePath )
+			->firejailDefaultSeccomp()
+			->disableNetwork()
+			->routeName( 'UTDRTweaks-optipng' )
+			->execute();
+		if ( $result->getExitCode() !== 0 ) {
+			$this->error( "Failed to process $fileName:\nout: {$result->getStdout()}\nerr: {$result->getStderr()}\n" );
+			return false;
+		}
+		$newSha1 = base_convert( sha1_file( $tempFilePath ), 16, 36, 31 );
+		if ( $oldSha1 === $newSha1 ) {
+			$this->output( "$fileName unchanged after processing; skipping update.\n" );
+			@unlink( $tempFilePath );
+			return false;
+		}
+		$newSize = filesize( $tempFilePath );
+		if ( !$dryRun ) {
+			// TODO: Update for the new file table schema.
+			$this->getPrimaryDB()
+				->newUpdateQueryBuilder()
+				->update( 'image' )
+				->set( [
+					'img_sha1' => $newSha1,
+					'img_size' => $newSize,
+				] )
+				->where([ 'img_name' => $fileName ])
+				->caller( __METHOD__ )
+				->execute();
+			$status = $this->repo->quickImport( $tempFilePath, $path );
+			if ( !$status->isGood() ) {
+				$this->error( $status );
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private function fixFileSize( LocalFile $file, bool $dryRun ): bool {
+		$oldSize = $file->getSize();
+		$newSize = $this->repo->getFileSize( $file->getPath() );
+		if ( $oldSize === $newSize ) {
+			return false;
+		}
+		$this->output( "Fixing file size for {$file->getName()}...\n" );
+		if ( !$dryRun ) {
+			$this->getPrimaryDB()
+				->newUpdateQueryBuilder()
+				->update( 'image' )
+				->set( [ 'img_size' => $newSize ] )
+				->where( [ 'img_name' => $file->getName() ] )
+				->caller( __METHOD__ )
+				->execute();
+		}
+		return true;
 	}
 }
 
